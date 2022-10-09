e6c035f5acfd790e1b784f9b33a89f06b0b55033
Fagner Ferreira <84201632+fagner02@users.noreply.github.com>
2022-10-09 20:13:10 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -115,6 +115,7 @@ class Home extends Component {
             style={{
               display: "flex",
               position: "relative",
+              flexWrap: "wrap",
               top: "-250px",
               height: "250px",
               transition: "top 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275)",
