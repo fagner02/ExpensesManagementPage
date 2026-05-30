ee05ef47773ce1c1d05c5f64bd7a035f3e136d3a
Fagner Ferreira <fagnerrc12@gmail.com>
2026-05-30 12:30:32 -0300

    remove unused file

--- src/lib/windowResize.ts
+++
@@ -1,6 +0,0 @@
-import { setHeightCallbacks } from "./store/store";
-
-document.addEventListener("resize", () => {
-    console.log("resize");
-    setHeightCallbacks.forEach((item) => item.cb());
-});
